ce4e451891273c8e47aaa16cb6886ceff57cf6aa
Devashish Ranjan <devashishranjan7@gmail.com>
2022-03-25 00:43:56 +0530

    header file inclusion done

--- pre_processor.c
+++ pre_processor.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 void print(FILE *);
 void copy_into_other_file(FILE *, FILE *);
 int f_size(FILE *);
 void copy_into_string(FILE *,char *);
 char *remove_comment(char *,FILE *,int);
+void fetch_word(char *, char *);
+void header_include(char *, char *, char *);
 void main()
 {
 	char fname[100];
@@ -54,7 +57,25 @@ void main()
 	for(int i=0;i<fsize;i++)
 		temp[i]=0;
 	copy_into_string(fp1,temp);
+	rewind(fp1);
+	char header[100];
+	fetch_word(temp,header);
+	printf("%s\n",header);
+
+	fp2=fopen("/usr/include/stdio.h","r");
+	int header_size=f_size(fp2);
+	rewind(fp2);
+	char *temp_header=malloc(header_size+10);
+	copy_into_string(fp2,temp_header);
+	rewind(fp2);
+
+	temp=realloc(temp,header_size);
+
+	header_include(temp,header,temp_header);
+	rewind(fp2);
+
 	printf("%s\n",temp);
+
 
 }
 
@@ -133,8 +154,31 @@ char *remove_comment(char *temp, FILE *fp,int fsize)
 /*-------------fetch_header_word----------------------------*/
 void fetch_word(char *temp, char *header)
 {
+	int i,j,k,l,h;
+
+	for(i=0;temp[i];i++)
+	{
+		if(temp[i]=='#')
+			k=i;
+		if(temp[i]=='>')
+			l=i;
+	}
+	for(j=k,h=0;j<=l;j++,h++)
+		header[h]=temp[j];
 
 }
 
 /*---------------header file include------------------------*/
-// void header_include()
+void header_include(char *temp, char *header, char *temp_header)
+{
+	char *addr=strstr(temp,header);
+	if(addr==NULL)
+		exit(1);
+	
+	else
+	{
+		memmove(addr+strlen(temp_header),addr+strlen(header),strlen(addr)-strlen(header)+1);
+		memcpy(addr,temp_header,strlen(temp_header));
+	}
+	printf("%s\n",temp);
+}
